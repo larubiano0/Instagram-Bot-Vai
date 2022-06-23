e0160a98efb2a22839dd2d358923106559c4c7ad
larubiano0 <larubiano0@gmail.com>
2022-06-23 02:34:24 -0500

    updated .gitignore

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from config import *
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import shapefile as shp
-import seaborn as sns
+
 
 df = pd.read_csv(data_dir)
 
-print(df)
+print(df.head())
